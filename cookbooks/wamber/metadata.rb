1d1c81ddc03d4ee97b12f215ba54511ebc9ae2f0
William McVey <wam@cisco.com>
2012-10-22 10:08:54 -0400

    Initial checkin of wamber cookbook&recipes

---
+++ cookbooks/wamber/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "William McVey"
+maintainer_email "wam@wamber.net"
+license          "Apache 2.0"
+description      "Installs/Configures wamber"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+require		 "python"
