2ec3ca7e1efa3039744a34ebcd1b86fff625e23c
William McVey <wam@cisco.com>
2012-10-24 18:36:59 -0400

    added Java cookbook for installation of ORacle Java. Added Dropbox cookbook for install Dropbox. And added java-plugin recipe for setting preferred plugin

--- roles/wamber-desktop.rb
+++ roles/wamber-desktop.rb
@@ -1,7 +1,18 @@
 name "wamber-desktop"
 description "Role for desktop (machines with a graphical interface) systems"
+
+override_attributes(
+	"java" => {
+		"install_flavor" => "oracle",
+		"oracle" => {
+			'accept_oracle_download_terms' => true
+		}
+	}
+)
 run_list(
 	"recipe[wamber::base]",
 	"recipe[wamber::desktop-base]",
-	"recipe[dropbox]"
+	"recipe[dropbox]",
+	"recipe[java]",
+	"recipe[wamber::java-plugin]"
 ) 
