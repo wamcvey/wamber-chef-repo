333b92016ce41c82de36ca01e6e890bc27e609f8
William McVey <wam@cisco.com>
2012-10-28 20:48:45 -0400

    Added chef-client::service

--- roles/wamber-base.rb
+++ roles/wamber-base.rb
@@ -13,11 +13,17 @@ override_attributes(
 		"oracle" => {
 			'accept_oracle_download_terms' => true
 		}
+	},
+	"chef_client" => {
+		"server_url" => "http://chef.home.wamber.net:4000/",
+		"cache_path" => "/var/cache/chef",
+		"backup_path" => "/var/lib/chef/backup"
 	}
 )
 run_list(
 	"recipe[wamber::base]",
 	"recipe[networking_basic]",
 	"recipe[netinfo]",
+	"recipe[chef-client::service]",
 	"recipe[java]"
 ) 
