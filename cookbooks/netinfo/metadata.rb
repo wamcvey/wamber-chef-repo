69b70ffd7ed246a34642a1d6a2a64fb802507d91
William McVey <wam@cisco.com>
2012-10-28 20:40:00 -0400

    Added new netinfo cookbook for assigning/setting address assignments from a databag

---
+++ cookbooks/netinfo/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "William McVey"
+maintainer_email "wam@wamber.net"
+license          "Apache 2.0"
+description      "Installs/Configures netinfo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends		"network_interfaces"
---
+++ cookbooks/netinfo/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: netinfo
+# Recipe:: defaul
+#
+# Copyright 2012, William McVey
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "network_interfaces"
+
+net_db = data_bag_item("netinfo", node[:netinfo][:bag])
+host_info = net_db["hosts"][node[:hostname]]
+if host_info 
+	net_info = net_db["networks"][host_info["network"]]
+	network_interfaces host_info["interface"] do
+		target  host_info["address"]
+		mask    net_info["netmask"]
+		network net_info["network"]
+		gateway net_info["gateway"]
+	end
+end
